b3cc8fa6cffa7b2648553e3335469a6ee11cd1f4
Abdoulaye Ndiaye <wizlaye7@gmail.com>
2021-04-07 19:05:46 +0000

    #

--- src/app/views/admin/admin-routing.module.ts
+++ src/app/views/admin/admin-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
+import { AjoutRapporteurComponent } from './ajout-rapporteur/ajout-rapporteur.component';
 
 const routes: Routes = [
   {
@@ -18,6 +19,13 @@ const routes: Routes = [
         component: AdminComponent,
         data: {
           title: 'admin'
+        }
+      },
+      {
+        path: 'ajout-rapporteur',
+        component: AjoutRapporteurComponent,
+        data: {
+          title: 'ajout-rapporteur'
         }
       },
     ]
